2240323451364fdcde011856dc8cfc834c32e550
Kyle Hilton <kylehilton92@gmail.com>
2015-05-13 10:11:38 -0700

    changes to app.js

--- appIonic/www/js/app.js
+++ appIonic/www/js/app.js
@@ -3,8 +3,62 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('headcount', [
+  'ionic',
+  'headcount.services',
+  'headcount.events',
+  'headcount.accounts',
+  'headcount.auth',
+  'ngRoute',
+  'ngMaterial',
+  'ui.bootstrap'
+])
+.config(function($stateProvider){
+  $stateProvider
+    .state('signin', {
+      url: '/signin',
+      templateUrl: '../templates/signin.html',
+      controller: 'AuthController'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: '../templates/signup.html',
+      controller: 'AuthController'
+    })
+    .state('event', {
+      url: '/event',
+      templateUrl: '../templates/event.html',
+      controller: 'eventsController',
+      authenticate: true
+    })
+    .state('events', {
+      url: '/events',
+      templateUrl: '../templates/eventslist.html',
+      controller: 'EventsController',
+      authenticate: true
+    })
+    .state('newevent', {
+      url: '/newevent',
+      templateUrl: '../templates/newevent.html',
+      controller: 'EventsController',
+      authenticate: true
+    })
+    .state('accounts', {
+      url: '/accounts',
+      templateUrl: '../templates/accounts.html',
+      controller: 'AccountsController',
+      authenticate: true
+    })
+    .otherwise({
+      redirectTo: '/events'
+    });
+})
+.factory('EventsFactory', function ($rootScope) {
 
+})
+.factory('AttachTokens', function ($window, $location) {
+
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
